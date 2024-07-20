87791e7b4165b4e1566185d8f22103b376c05856
emanueleizzo <emanuele.izzo_1998@outlook.it>
2024-07-20 13:36:52 +0200

    Modifiche varie

--- js/database.js
+++ js/database.js
@@ -11,7 +11,8 @@ const tables = {
         Conversioni: { Materia: "informatica", Nome: "sulle conversioni", Descrizione: "Conversioni da decimale ad altre basi, conversioni da altre basi a decimale, e conversione tra diverse basi." },
         IEEE_754: { Materia: "informatica", Nome: "sull'IEEE 754", Descrizione: "Defizione della rappresentazione in virgola mobile e sul come calcolare la conversione di un numero con l'IEEE 754." },
 
-        Macchina_Von_Neumann: { Materia: "sistemi_operatvi", Nome: "sulla macchina di Von Neumann", Descrizione: "Definizione della macchina e di tutti i suoi componenti." },
+        Macchina_Von_Neumann: { Materia: "architettura_pc", Nome: "sulla macchina di Von Neumann", Descrizione: "Definizione della macchina e di tutti i suoi componenti." },
+        Microprocessore: { Materia: "architettura_pc", Nome: "sul microprocessore", Descrizione: "Struttura del microprocessore e dei suoi componenti, definizione di ciclo macchina e di pipeline. A cura di Giulia Nora (3O, A.A. 2023/24)." },
 
         ISO_OSI_TCP_IP: { Materia: "reti_calcolatori", Nome: "sul modello ISO/OSI e lo stack TCP/IP", Descrizione: "Definizione del modello ISO/OSI, dello stack TCP/IP, dell'incapsulamento e dei PDU." },
         Livello_rete: { Materia: "reti_calcolatori", Nome: "sul livello di rete", Descrizione: "Introduzione sul livello di rete e sul protocollo IP, definizione dell'indirizzo IP e del routing." },
@@ -31,6 +32,7 @@ const tables = {
         Rompicapo_8_Regine: { Nome: "per la risoluzione del Rompicapo delle 8 Regine", Descrizione: "Definizione del problema e realizzazione di due codici: uno in Python in grado di generare tutte le soluzioni possibili data una scacchiera di lato \\(k\\) e uno in Prolog in grado di verificare che una soluzione sia valida." },
         Passeggiata_Cavallo: { Nome: "per la risoluzione della Passeggiata del Cavallo", Descrizione: "Definizione del problema e realizzazione di due codici: uno in Python in grado di generare tutte le soluzioni possibili data una scacchiera di lato \\(k\\) e uno in Prolog in grado di verificare che una soluzione sia valida." },
         Equazione_Matematica: { Nome: "per la risoluzione del problema dell'Equazione Matematica", Descrizione: "Definizione del problema e realizzazione di due codici: uno in Python in grado di generare tutte le soluzioni possibili e uno in Prolog in grado di verificare che una soluzione sia valida." },
+        Quadrato_Magico: { Nome: "per la risoluzione del quadrato magico", Descrizione: "Definizione del problema e realizzazione di due codici: uno in Python in grado di generare tutte le soluzioni possibili e uno in Prolog in grado di verificare che una soluzione sia valida." },
     }
 };
 
