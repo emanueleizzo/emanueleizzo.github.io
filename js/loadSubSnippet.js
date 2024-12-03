1212718050186b1c851045127134998a5616820b
emanueleizzo <79841635+emanueleizzo@users.noreply.github.com>
2024-12-03 21:44:37 +0100

    Versione 2.0 reset

--- js/loadSubSnippet.js
+++
@@ -1,9 +0,0 @@
-function loadSubSnippet(subject) {
-    var path = "html/subject/" + subject + ".html";
-
-    fetch(path)
-        .then(response => response.text())
-        .then((data) => {
-            document.getElementById("subject").innerHTML = data;
-        });
-}
--- js/showSubSnippet.js
+++
@@ -1,4 +0,0 @@
-function showSubSnippet(id_list, id_target) {
-    id_list.forEach((id) => document.getElementById(id).style.display = "none");
-    document.getElementById(id_target).style.display = "block";
-}
